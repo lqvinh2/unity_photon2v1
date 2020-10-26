3cf4b5d15318c7b0074584b36131feeb55a2080c
lamquangvinh <lqvinh.hsu@gmail.com>
2020-10-27 02:02:54 +0700

    Learning 1

---
+++ Assets/VV/C#_Learning/SinCoSTanCTan.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinCoSTanCTan : MonoBehaviour
+{
+    enum aa{ 
+      sin, cos, tan, cotan, moveCircle
+    }
+
+    [SerializeField]
+    aa SCTCT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    [SerializeField]
+    float amplitude = 3;
+
+    [SerializeField]
+    float frequnce = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x, y, z;
+
+        switch (SCTCT)
+        {
+            case aa.sin:
+
+                x = transform.position.x;
+                y = Mathf.Sin(Time.time * frequnce) * amplitude;
+                z = transform.position.z;
+                transform.position = new Vector3(x,y,z);
+                break;
+            case aa.cos:
+                x = Mathf.Sin(Time.time * frequnce) * amplitude;
+                y = transform.position.y;
+                z = transform.position.z;
+                transform.position = new Vector3(x, y, z);
+
+                break;
+            case aa.tan:
+
+                x = transform.position.x;
+                y = Mathf.Tan(Time.time);
+                z = transform.position.z;
+                transform.position = new Vector3(x, y, z);
+                break;
+            case aa.cotan:
+                break;
+            case aa.moveCircle:
+                x = Mathf.Cos(Time.time * frequnce) * amplitude;
+                y = Mathf.Sin(Time.time * frequnce) * amplitude;
+                z = transform.position.z;
+                transform.position = new Vector3(x, y, z);
+                break;
+            default:
+                break;
+        }
+        
+
+        Debug.Log(Time.time);
+    }
+}
---
+++ Assets/VV/C#_Learning/learning.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class learning : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform enemy;
+
+    [SerializeField]
+    float move_speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction = enemy.position - transform.position;
+        
+        // assume that we have : direction x = 4, y = 5 => a = 1.25
+        float atan2 = Mathf.Atan2(direction.y, direction.x);
+        float angle = atan2 * Mathf.Rad2Deg - 90;
+
+        Debug.DrawRay(transform.position, direction, Color.green);
+    
+        //Debug.Log(atan2);
+        //Debug.Log(angle);
+
+        Quaternion angleAxist = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, angleAxist, Time.deltaTime * 2);
+
+        //var direction = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)) - transform.position;
+        //transform.eulerAngles = Vector3.forward * angle;
+        direction.Normalize();
+        Debug.DrawRay(transform.position, direction, Color.red);
+        transform.Translate(direction * Time.deltaTime * move_speed);
+
+    }
+}
